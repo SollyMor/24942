38e59095d34a05b317c0061146d71be451f9af1e
Soffa <Solmkey@gmail.com>
2025-11-26 16:04:30 +0700

    ex_32.6

--- s.morozova/32/client.c
+++ s.morozova/32/client.c
@@ -5,20 +5,13 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 
-#define SOCKET_PATH "/tmp/case_converter_epoll_socket"
+#define SOCKET_PATH "/tmp/async_socket"
 #define BUFFER_SIZE 1024
 
-int main(int argc, char *argv[]) {
+int main() {
     int sockfd;
-    struct sockaddr_un server_addr;
+    struct sockaddr_un addr;
     char buffer[BUFFER_SIZE];
-    
-    if (argc < 2) {
-        printf("Использование: %s <client_name>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-    
-    char *client_name = argv[1];
     
     // Создаем сокет
     sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
@@ -28,38 +21,27 @@ int main(int argc, char *argv[]) {
     }
     
     // Настраиваем адрес сервера
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sun_family = AF_UNIX;
-    strncpy(server_addr.sun_path, SOCKET_PATH, sizeof(server_addr.sun_path) - 1);
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
     
     // Подключаемся к серверу
-    if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+    if (connect(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
         perror("connect");
         close(sockfd);
         exit(EXIT_FAILURE);
     }
     
-    printf("Клиент '%s' подключен к серверу\n", client_name);
-    
-    // Читаем приветственное сообщение
-    ssize_t bytes_read = read(sockfd, buffer, BUFFER_SIZE - 1);
-    if (bytes_read > 0) {
-        buffer[bytes_read] = '\0';
-        printf("Сервер: %s", buffer);
-    }
+    printf("Подключен к серверу\n");
     
     // Отправляем несколько сообщений
     for (int i = 1; i <= 3; i++) {
-        snprintf(buffer, BUFFER_SIZE, "Сообщение %d от клиента %s\n", i, client_name);
-        
-        printf("Отправляю: %s", buffer);
+        snprintf(buffer, BUFFER_SIZE, "Сообщение %d от клиента\n", i);
         write(sockfd, buffer, strlen(buffer));
-        
-        // Ждем ответа
+        printf("Отправлено: %s", buffer);
         sleep(1);
     }
     
-    printf("Клиент '%s' завершает работу\n", client_name);
     close(sockfd);
     return 0;
 }
--- s.morozova/32/server.c
+++ s.morozova/32/server.c
@@ -4,207 +4,52 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <sys/epoll.h>
-#include <ctype.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <time.h>
 
-#define SOCKET_PATH "/tmp/case_converter_epoll_socket"
+#define SOCKET_PATH "/tmp/async_socket"
 #define BUFFER_SIZE 1024
-#define MAX_EVENTS 10
-#define MAX_CLIENTS 10
 
-typedef struct {
-    int fd;
-    char read_buffer[BUFFER_SIZE];
-    int active;
-    int client_id;
-} client_t;
-
-client_t clients[MAX_CLIENTS];
-int server_fd;
-int epoll_fd;
-int client_counter = 0;
-
-void cleanup() {
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (clients[i].active) {
-            close(clients[i].fd);
-        }
+int main(int argc, char *argv[]) {
+    int sockfd;
+    struct sockaddr_un addr;
+    char buffer[BUFFER_SIZE];
+    
+    if (argc < 2) {
+        printf("Использование: %s <текст для отправки>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
     
-    if (epoll_fd >= 0) close(epoll_fd);
-    if (server_fd >= 0) close(server_fd);
-    unlink(SOCKET_PATH);
-}
-
-void handle_client_data(int client_fd) {
-    client_t *client = NULL;
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (clients[i].active && clients[i].fd == client_fd) {
-            client = &clients[i];
-            break;
-        }
-    }
-    
-    if (!client) return;
-
-    ssize_t bytes_read = read(client_fd, client->read_buffer, BUFFER_SIZE - 1);
-    
-    if (bytes_read > 0) {
-        // Преобразуем в верхний регистр и выводим - ЭТО ГЛАВНАЯ ЦЕЛЬ!
-        printf("[Клиент %d исходный]: ", client->client_id);
-        for (int i = 0; i < bytes_read; i++) {
-            putchar(client->read_buffer[i]);
-        }
-        
-        printf("[Клиент %d результат]: ", client->client_id);
-        for (int i = 0; i < bytes_read; i++) {
-            putchar(toupper(client->read_buffer[i]));
-        }
-        fflush(stdout);
-        
-    } else if (bytes_read == 0) {
-        printf("Клиент %d отключен\n", client->client_id);
-        
-        struct epoll_event ev;
-        epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, &ev);
-        
-        close(client_fd);
-        client->active = 0;
-        
-    } else {
-        if (errno != EWOULDBLOCK && errno != EAGAIN) {
-            struct epoll_event ev;
-            epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, &ev);
-            close(client_fd);
-            client->active = 0;
-        }
-    }
-}
-
-void accept_new_client() {
-    int new_socket = accept(server_fd, NULL, NULL);
-    if (new_socket == -1) {
-        if (errno != EWOULDBLOCK && errno != EAGAIN) {
-            perror("accept");
-        }
-        return;
-    }
-    
-    int flags = fcntl(new_socket, F_GETFL, 0);
-    if (fcntl(new_socket, F_SETFL, flags | O_NONBLOCK) == -1) {
-        perror("fcntl");
-        close(new_socket);
-        return;
-    }
-    
-    int added = 0;
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        if (!clients[i].active) {
-            clients[i].fd = new_socket;
-            clients[i].active = 1;
-            clients[i].client_id = ++client_counter;
-            
-            struct epoll_event ev;
-            ev.events = EPOLLIN | EPOLLET;
-            ev.data.fd = new_socket;
-            
-            if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, new_socket, &ev) == -1) {
-                perror("epoll_ctl");
-                close(new_socket);
-                clients[i].active = 0;
-            } else {
-                printf("Клиент %d подключен\n", clients[i].client_id);
-                added = 1;
-            }
-            break;
-        }
-    }
-    
-    if (!added) {
-        close(new_socket);
-    }
-}
-
-int main() {
-    struct sockaddr_un server_addr;
-    struct epoll_event events[MAX_EVENTS];
-    
-    atexit(cleanup);
-    
-    for (int i = 0; i < MAX_CLIENTS; i++) {
-        clients[i].active = 0;
-        clients[i].fd = -1;
-    }
-    
-    server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (server_fd == -1) {
+    // Создаем сокет
+    sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sockfd == -1) {
         perror("socket");
         exit(EXIT_FAILURE);
     }
     
-    int flags = fcntl(server_fd, F_GETFL, 0);
-    if (fcntl(server_fd, F_SETFL, flags | O_NONBLOCK) == -1) {
-        perror("fcntl");
-        close(server_fd);
+    // Настраиваем адрес сервера
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
+    
+    // Подключаемся к серверу
+    if (connect(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+        perror("connect");
+        close(sockfd);
         exit(EXIT_FAILURE);
     }
     
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sun_family = AF_UNIX;
-    strncpy(server_addr.sun_path, SOCKET_PATH, sizeof(server_addr.sun_path) - 1);
+    printf("Подключен к серверу\n");
     
-    unlink(SOCKET_PATH);
+    // Отправляем текст
+    snprintf(buffer, BUFFER_SIZE, "%s\n", argv[1]);
+    ssize_t bytes_sent = write(sockfd, buffer, strlen(buffer));
     
-    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-        perror("bind");
-        close(server_fd);
-        exit(EXIT_FAILURE);
+    if (bytes_sent > 0) {
+        printf("Отправлено серверу: %s", buffer);
     }
     
-    if (listen(server_fd, 5) == -1) {
-        perror("listen");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
+    // Ждем немного перед закрытием
+    sleep(1);
     
-    epoll_fd = epoll_create1(0);
-    if (epoll_fd == -1) {
-        perror("epoll_create1");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
-    
-    struct epoll_event ev;
-    ev.events = EPOLLIN;
-    ev.data.fd = server_fd;
-    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &ev) == -1) {
-        perror("epoll_ctl");
-        close(epoll_fd);
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
-    
-    printf("Сервер запущен. Ожидание подключений...\n");
-    
-    while (1) {
-        int nfds = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
-        
-        if (nfds == -1) {
-            perror("epoll_wait");
-            break;
-        }
-        
-        for (int i = 0; i < nfds; i++) {
-            if (events[i].data.fd == server_fd) {
-                accept_new_client();
-            } else {
-                handle_client_data(events[i].data.fd);
-            }
-        }
-    }
-    
+    close(sockfd);
     return 0;
 }
